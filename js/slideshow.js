17b805e8d539f967a4fd778862b9abeecda2da04
Egbodor CollinsS <120804886+futuristhurs@users.noreply.github.com>
2023-03-14 19:28:27 +0100

    Add files via upload

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,15 +1,10 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var slides = document.getElementsByClassName("slide");
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].classList.remove("active");
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex-1].classList.add("active");
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5,
+  },
+});
